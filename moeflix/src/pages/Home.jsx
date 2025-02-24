53eacce3504d32359d308baaceff355682d57f90
Jemima <136545759+jweld-codes@users.noreply.github.com>
2025-02-24 02:43:15 -0600

    Search the movie
    Made the search for a movie function work

--- moeflix/src/pages/Home.jsx
+++ moeflix/src/pages/Home.jsx
@@ -28,9 +28,23 @@ function Home(){
         loadPopularMovies()
     }, []);
 
-    const handleSearch =(e)=>{
+    const handleSearch = async(e)=>{
         e.preventDefault()
-        alert(searchQuery)
+        if(!searchQuery.trim()) return
+        if(loading)return
+
+        setLoading(true)
+        try{
+            const searchResults = await searchMovies(searchQuery);
+            setMovies(searchResults)
+            setError(null)
+        }catch(err){
+            console.log(err);
+            setError("Failed to load movie...")
+        } finally {
+            setLoading(false)
+        }
+
         // setSearchQuery("---")
     };
 
@@ -47,14 +61,19 @@ function Home(){
             <button type="submit" className="search-btn">Search</button>
         </form>
 
-        <div className="movies-grid">
-            {movies.map(
-                (movie) =>
-                /*movie.title.toLowerCase().startsWith(searchQuery) &&*/ (
-                <MovieCard movie={movie} key={movie.id} />
-                )
-            )}
-        </div>
+        {error && <div className="error-message">{error}</div> }
+        {loading? (
+            <div className="loading">Loading...</div>
+        ) : (
+            <div className="movies-grid">
+                {movies.map(
+                    (movie) =>
+                    /*movie.title.toLowerCase().startsWith(searchQuery) &&*/ (
+                    <MovieCard movie={movie} key={movie.id} />
+                    )
+                )}
+            </div>
+        ) }
     </div>
 }
 
